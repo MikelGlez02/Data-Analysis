ea82c1e1d1e0a2c6c41d65a7d8d86cf886e32c9d
MikelGlez02 <96484687+MikelGlez02@users.noreply.github.com>
2024-12-13 10:50:47 +0100

    Update producer.py

--- Proyecto_TD/kafka/producer.py
+++ Proyecto_TD/kafka/producer.py
@@ -1,1 +1,15 @@
+# kafka/producer.py
+from kafka import KafkaProducer
+import json
 
+class KafkaRecipeProducer:
+    def __init__(self, topic, bootstrap_servers="localhost:9092"):
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8")
+        )
+        self.topic = topic
+
+    def send_message(self, message):
+        self.producer.send(self.topic, value=message)
+        print(f"Message sent to topic {self.topic}: {message}")
