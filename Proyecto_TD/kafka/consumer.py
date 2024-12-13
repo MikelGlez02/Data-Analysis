db26f116adb6a860d3f9a731a801cbb6468c3bce
MikelGlez02 <96484687+MikelGlez02@users.noreply.github.com>
2024-12-13 10:51:12 +0100

    Update consumer.py

--- Proyecto_TD/kafka/consumer.py
+++ Proyecto_TD/kafka/consumer.py
@@ -1,1 +1,15 @@
+# kafka/consumer.py
+from kafka import KafkaConsumer
+import json
 
+class KafkaRecipeConsumer:
+    def __init__(self, topic, bootstrap_servers="localhost:9092"):
+        self.consumer = KafkaConsumer(
+            topic,
+            bootstrap_servers=bootstrap_servers,
+            value_deserializer=lambda v: json.loads(v.decode("utf-8"))
+        )
+
+    def consume_messages(self):
+        for message in self.consumer:
+            print(f"Message received from topic {message.topic}: {message.value}")
