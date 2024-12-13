679fe1c807a237ddc940cf21efce05e56bc9ca15
MikelGlez02 <96484687+MikelGlez02@users.noreply.github.com>
2024-12-13 09:44:19 +0100

    Update logger.py

--- Proyecto_TD/src/utils/logger.py
+++ Proyecto_TD/src/utils/logger.py
@@ -1,1 +1,10 @@
+import logging
 
+def setup_logger(level):
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.StreamHandler()
+        ]
+    )
