ca3cd2816d5433e0172ca4eef205ce42f945a102
MikelGlez02 <96484687+MikelGlez02@users.noreply.github.com>
2024-12-14 07:10:19 +0100

    Update test_preprocessing.py

--- Proyecto_TD/tests/test_preprocessing.py
+++ Proyecto_TD/tests/test_preprocessing.py
@@ -1,1 +1,9 @@
+# tests/test_preprocessing.py
+from preprocessing.text_cleaner import clean_text
 
+def test_clean_text():
+    input_text = "This is a TEST text with punctuation!"
+    expected_output = "test text punctuation"
+    cleaned_text = clean_text(input_text)
+    assert cleaned_text == expected_output, f"Expected {expected_output}, but got {cleaned_text}"
+    print("Preprocessing test passed.")
