e11b7e93a03a93a54dfbd0d34c1849f619beae9d
MikelGlez02 <96484687+MikelGlez02@users.noreply.github.com>
2024-12-14 08:18:39 +0100

    Update logger.py

--- Proyecto_TD/src/utils/logger.py
+++ Proyecto_TD/src/utils/logger.py
@@ -1,10 +1,8 @@
+# utils/logger.py
 import logging
+import os
 
-def setup_logger(level):
-    logging.basicConfig(
-        level=level,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers=[
-            logging.StreamHandler()
-        ]
-    )
+def setup_logger():
+    log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+    logging.basicConfig(level=log_level, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", handlers=[logging.StreamHandler()])
+
